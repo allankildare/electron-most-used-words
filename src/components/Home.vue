b0365b56ee438140ae892cd19e9b50cfafe38d0e
allankildare <allankildare27@gmail.com>
2021-05-24 21:04:41 -0300

    + interface grafica

---
+++ src/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input
+          label="Selecione as legendas"
+          prepend-icon="mdi-message-text"
+          outlined
+          append-icon="mdi-send"
+          multiple
+          chips
+          v-model="files"
+          @click:append-outer="processSubtitles" />
+      </v-form>
+
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name" 
+            :name="word.name" :amount="word.amount" />
+      </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data: function() {
+        return {
+            files: [],
+            groupedWords: [
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 956 },
+                { name: 'they', amount: 50 },
+                { name: 'we', amount: 740 },
+            ]
+        }    
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style>
+    .pills {
+        display: flex;    
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="pill">
+      <span class="name">{{ name }}</span>
+      <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+}
+</script>
+
+<style scoped>
+    .pill {
+        color: #fff;
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+        background-color: #0687d3;
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .pill .name {
+        margin-left: 15px;
+
+    }
+
+    .pill .amount {
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        width: 35px;
+        border-radius: 17.5px;
+
+        background-color: white;
+        color: #0687d3;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
